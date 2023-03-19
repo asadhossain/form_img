a1f7ae006610a217555e665b0b628e87a9794a0c
asadhossain <developerasad007@gmail.com>
2023-03-20 02:42:37 +0600

    Asad

--- img.php
+++ img.php
@@ -11,3 +11,23 @@
 
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image viewport</title>
+</head>
+<body>
+    
+
+<img src="upload/<?php if(isset($img_name)) {echo $img_name;} ?>" alt="">
+
+
+
+
+</body>
+</html>
